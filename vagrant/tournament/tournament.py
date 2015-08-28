d64552a6beeefd0fc16bf10873298085cb6861a6
marzukt <marzukt@gmail.com>
2015-08-28 16:08:14 +0100

    Add delete functions

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,10 +13,20 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    db = connect()
+    c = db.cursor()
+    c.execute('TRUNCATE TABLE matches;')
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute('TRUNCATE TABLE players;')
+    db.commit()
+    db.close()
+
 
 
 def countPlayers():
@@ -25,10 +35,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +66,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,9 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-
-
+CREATE DATABASE tournament;
+CREATE TABLE players ( id SERIAL,
+                       player_name TEXT);
+CREATE TABLE matches (match_id SERIAL,
+                      player1_id int references players(id),
+                      player2_id int references players(id));
